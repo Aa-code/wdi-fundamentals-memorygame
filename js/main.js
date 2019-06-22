8b4e49f76961eecd0abcbc5887b9abbc5e575f42
Aa-code <Aliyahdevcode@outlook.com>
2019-06-22 18:44:49 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+
+console.log("User flipped " + cardsInPlay);
+console.log("User flipped " + cardsInPlay);
+
+if (cardsInPlay[cardsInPlay.length] === 2);
+{
+  if (cardsInPlay[0] === cardsInPlay[1])
+    alert("You found a match!");
+  else if (cardsInPlay != cardsInPlay[1]);
+    alert("Sorry, try again.");
+}
